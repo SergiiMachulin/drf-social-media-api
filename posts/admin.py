6a2187abca6765eb86800924db0e152ad68bc242
Sergii Machulin <machulinsv@gmail.com>
2023-04-21 18:12:37 +0300

    Added posts models and migrations

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, Hashtag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "content", "created_at")
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to="post_image", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    hashtags = models.ManyToManyField(Hashtag, blank=True)
+
+    class Meta:
+        default_related_name = "posts"
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.content
