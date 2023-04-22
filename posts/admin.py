2bc27e2978f206519ec8e077c681cef2af8ab4be
Sergii Machulin <machulinsv@gmail.com>
2023-04-22 17:32:53 +0300

    Updated post admin.py with filtering

--- posts/admin.py
+++ posts/admin.py
@@ -6,8 +6,12 @@ from posts.models import Post, Hashtag
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("id", "author", "content", "created_at")
+    list_filter = ("hashtags",)
 
 
 @admin.register(Hashtag)
 class HashtagAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
+
+    class Meta:
+        ordering = ["name"]
