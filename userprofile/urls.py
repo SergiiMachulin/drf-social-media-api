fce64c518a7d9f56fe71425c09322ffe306ed914
Sergii Machulin <machulinsv@gmail.com>
2023-04-22 20:04:55 +0300

    Updated user profile urls

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,14 +6,7 @@ from .views import UserProfileList, UserProfileDetail, UserProfileUpdate
 urlpatterns = [
     path("", UserProfileList.as_view(), name="profile-list"),
     path("<int:pk>/", UserProfileDetail.as_view(), name="profile-detail"),
-    path("update/", UserProfileUpdate.as_view(), name="profile-update"),
+    path("<int:pk>/update/", UserProfileUpdate.as_view(), name="profile-update"),
 ]
-
-urlpatterns += format_suffix_patterns(
-    [
-        path("", UserProfileList.as_view(), name="profile-list"),
-    ]
-)
-
 
 app_name = "profile"
