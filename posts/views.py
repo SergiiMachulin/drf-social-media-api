fd29c80033ecb632ed89e6303a2fa36394f18d71
Sergii Machulin <machulinsv@gmail.com>
2023-04-23 23:19:34 +0300

    Added pagination on following posts page

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.db.models import Q
+from django.db.models import Q, QuerySet
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -10,18 +11,12 @@ from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    """Retrieve own posts and posts of users are followed by. Also, it is available to retrieve posts by hashtags"""
+
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
-    @action(detail=False)
-    def following(self, request) -> Response:
-        queryset = Post.objects.filter(user__followers=request.user)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = Post.objects.filter(Q(user=user) | Q(user__in=user.following.all()))
+    def filter_by_hashtags(self, queryset):
         hashtags = self.request.query_params.get("hashtags")
         if hashtags:
             hashtags = hashtags.split(",")
@@ -29,5 +24,22 @@ class PostViewSet(viewsets.ModelViewSet):
                 queryset = queryset.filter(hashtags__contains=f"#{hashtag}")
         return queryset
 
-    def perform_create(self, serializer):
+    @action(detail=False, pagination_class=PageNumberPagination)
+    def following(self, request) -> Response:
+        queryset = Post.objects.filter(user__in=self.request.user.user_followees.all())
+        queryset = self.filter_by_hashtags(queryset)
+        paginator = self.pagination_class()
+        queryset = paginator.paginate_queryset(queryset, request)
+        serializer = self.get_serializer(queryset, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def get_queryset(self) -> QuerySet:
+        followees = self.request.user.user_followees.all()
+        queryset = Post.objects.filter(
+            Q(user=self.request.user) | Q(user__in=followees)
+        )
+        queryset = self.filter_by_hashtags(queryset)
+        return queryset
+
+    def perform_create(self, serializer) -> None:
         serializer.save(user=self.request.user)
